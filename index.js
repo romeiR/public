2b8991f274332a017797ca4a0c6b63b5b9cd4f68
ROMEI <yuyama1629@gmail.com>
2021-04-11 22:07:15 +0900

    httpサーバー作りかけ

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const http = require('http');
 const router = require('./lib/router');
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
-    });
     router.route(req,res);
     res.end();
   })
